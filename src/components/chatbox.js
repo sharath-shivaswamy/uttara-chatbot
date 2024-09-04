cb5d79ddb7093cad6d0964122216ef04590b8bf7
nik <43464045+ph4nik@users.noreply.github.com>
2024-09-04 11:41:17 +0530

    improved chatbox section

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -13,30 +13,23 @@ export default function ChatBox() {
   };
 
   return (
-    <div className={styles.app}>
-      {/* <header className={styles.header}>
-        <h1>Chat Page</h1>
-      </header> */}
-      <div className={styles.mainContent}>
-        <div className={styles.chatBox}>
-          <h2>Research company</h2>
-          <form onSubmit={handleSubmit} className={styles.form}>
-            <textarea
-              value={inputValue}
-              placeholder="Message Uttara"
-              onChange={(e) => setInputValue(e.target.value)}
-              rows={2}
-              className={styles.textArea}
-            />
-            <button type="submit" className={styles.submitButton}>
-              Submit
-            </button>
-          </form>
-          <div className={styles.readOnlyOutput}>
-            <h3>Output:</h3>
-            <pre>{submittedValue}</pre>
-          </div>
-        </div>
+    <div className={styles.chatBox}>
+      <h2>Research company</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <textarea
+          value={inputValue}
+          placeholder="Message Uttara"
+          onChange={(e) => setInputValue(e.target.value)}
+          rows={2}
+          className={styles.textArea}
+        />
+        <button type="submit" className={styles.submitButton}>
+          Submit
+        </button>
+      </form>
+      <div className={styles.readOnlyOutput}>
+        <h3>Output:</h3>
+        <div className={styles.readOnlyOutputTextArea}>{submittedValue}</div>
       </div>
     </div>
   );
